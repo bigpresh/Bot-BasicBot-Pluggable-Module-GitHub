68a52441b417c8bd580a7827fedf5e9987b5bf7f
David Precious <davidp@preshweb.co.uk>
2011-10-03 18:11:30 +0100

    Use Net::GitHub::V2 to get commit/issue details.
    TODO: it would be nice to use Net::GitHub::V2 to fetch details of pull requests,
    too, but it doesn't seem that Net::GitHub::V2 exposes that ability yet.
    
    I'll try to find some time to submit a pull request to add that.

--- lib/Bot/BasicBot/Pluggable/Module/GitHub/EasyLinks.pm
+++ lib/Bot/BasicBot/Pluggable/Module/GitHub/EasyLinks.pm
@@ -31,9 +31,11 @@ sub said {
     
     return unless $pri == 2;
 
+
     # Loop through matching things in the message body, assembling quick links
     # ready to return.
     my @return;
+    match:
     while ($mess->{body} =~ m{ 
         (?:  
             # "Issue 42", "PR 42" or "Pull Request 42"
@@ -50,7 +52,11 @@ sub said {
     ) {
 
         my $project = $+{project} || $self->github_project($mess->{channel});
-        next unless $project;
+        return unless $project;
+
+        # Get the Net::GitHub::V2 object we'll be using.  (If we don't get one,
+        # for some reason, we can't do anything useful.)
+        my $ng = $self->ng or return;
 
         # First, extract what kind of thing we're looking at, and normalise it a
         # little, then go on to handle it.
@@ -67,15 +73,23 @@ sub said {
         # Right, handle it in the approriate way
         if ($thing =~ /Issue|GH/i) {
             warn "Handling issue $thingnum";
-            my $issue_url = "https://github.com/$project/issues/$thingnum";
-            my $title = URI::Title::title($issue_url);
-            $title =~ s/ - Issues - \Q$project\E - GitHub//;
-            push @return, "Issue $thingnum ($title) - $issue_url";
+            my $issue = $ng->issue->view($thingnum);
+            if (!$issue) {
+                push @return, "Issue $thingnum not found?";
+                next match;
+            }
+            push @return, sprintf "Issue %d (%s) - %s",
+                $thingnum,
+                $issue->{title},
+                $issue->{html_url};
         }
 
         # Similarly, pull requests:
         if ($thing =~ /(?:pr|pull request)/i) {
             warn "Handling pull request $thingnum";
+            # TODO: send a pull request to add support for fetching details of
+            # pull requests to Net::GitHub::V2, so we can handle PRs on private
+            # repos appropriately.
             my $pull_url = "https://github.com/$project/pull/$thingnum";
             my $title = URI::Title::title($pull_url);
             push @return, "Pull request $thingnum ($title) - $pull_url";
@@ -84,23 +98,23 @@ sub said {
         # If it was a commit:
         if ($thing eq 'commit') {
             warn "Handling commit $thingnum";
-            my $commit = JSON::from_json(
-                LWP::Simple::get(
-                    "https://github.com/api/v2/json/commits/show/$project/$thingnum"
-                )
-            );
-            # If we got nothing back, assume it wasn't actually a valid SHA; if we
-            # got details, use them
+            my $commit = $ng->commit->show($thingnum);
             if ($commit) {
-                my $commit = $commit->{commit};  # ugh.
-
-                # OK, take the first line of the commit message as a title:
-                my $summary = (split /\n/, $commit->{message} )[0];
-
-                my $commit_url = "https://github.com" . $commit->{url};
-                push @return, "Commit $thingnum ($summary) - $commit_url";
+                my $title = ( split /\n+/, $commit->{message} )[0];
+                my $url = $commit->{url};
+                
+                # Currently, the URL given doesn't include the host, but that
+                # might perhaps change in future, so play it safe:
+                $url = "https://github.com$url" unless $url =~ /^http/;
+                push @return, sprintf "Commit $thingnum (%s) - %s",
+                    $title,
+                    $url;
             } else {
-                warn "No commit details for $project/$thingnum";
+                # We purposefully don't show a message on IRC here, as we guess
+                # what might be a SHA, so we could be annoying saying that we
+                # didn't match a commit when someone said a word that just
+                # happened to look like it could be the start of a SHA.
+                warn "No commit details for $thingnum \@ $project/$thingnum";
             }
         }
     }
