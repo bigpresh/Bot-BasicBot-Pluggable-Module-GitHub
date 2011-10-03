ce79628f78370125c4937bfabcea65d9bef77ae6
David Precious <davidp@preshweb.co.uk>
2011-10-03 17:48:05 +0100

    Monster refactor - sane config, Net::GitHub.
    Provide a new !setgithubproject command to set the project and, optionally, auth
    details.
    
    Add new ng() method which returns an appropriately-configured Net::GitHub::V2
    object for the given channel's project details, so the rest of the code can use
    Net::GitHub::V2, avoid wheel re-invention, and benefit from authentication
    support.

--- lib/Bot/BasicBot/Pluggable/Module/GitHub.pm
+++ lib/Bot/BasicBot/Pluggable/Module/GitHub.pm
@@ -13,40 +13,113 @@ use strict;
 
 our $VERSION = '0.01';
 
+# We'll cache suitably-configured Net::GitHub objects for each channel.
+my %net_github;
 
-sub github_project {
+# Given a channel name, return a suitably-configured Net::GitHub::V2 object
+sub ng {
     my ($self, $channel) = @_;
+    if (my $ng = $net_github{$channel}) {
+        return $ng;
+    }
 
-    my $projects = $self->get('user_github_project')
-        or return;
+    my $chanproject = $self->project_for_channel($channel);
+    return unless $chanproject;
 
-    # The user may have provided channel-specific projects definitions in the
-    # format:
-    #  '#channel1:user/repo;#channel2:user/repo'
-    return $projects if ($projects !~ /:/);
+    my ($user, $project) = split '/', $chanproject, 2;
 
-    # Or, they may have simply provided a project name which should apply to
-    # all channels the bot is in.
-    my %repo_for_channel = map {
-        split /[:]/
-    } split /[;,]/, $projects;
-    return $repo_for_channel{$channel};
+    my %ngparams = (
+        owner => $user,
+        repo  => $project,
+    );
+
+    # If authentication is needed, add that in too:
+    if (my $auth = $self->auth_for_channel($channel)) {
+        my ($user, $token) = split /:/, $auth, 2;
+        $ngparams{login} = $user;
+        $ngparams{token} = $token;
+        $ngparams{always_Authorization} = 1;
+    }
+    return $net_github{$channel} = Net::GitHub::V2->new(%ngparams);
 }
 
 
-# For each channel the bot is in, call github_project() to find out what project
-# is appropriate for that channel, and return a hashref of channel => projet.
+# Find the name of the GitHub project for a given channel
+sub project_for_channel {
+    my ($self, $channel) = @_;
+
+    my $project_for_channel =
+        $self->store->get('GitHub', 'project_for_channel');
+    return $project_for_channel->{$channel};
+}
+# Alias for backwards compatibility
+sub github_project {
+    my $self = shift;
+    $self->project_for_channel(@_);
+}
+
+# Find auth details to use to access a channel's project
+sub auth_for_channel {
+    my ($self, $channel) = @_;
+
+    my $auth_for_channel = 
+        $self->store->get('GitHub', 'auth_for_channel');
+    return $auth_for_channel->{$channel};
+}
+
+
+# For each channel the bot is in, call project_for_channel() to find out what 
+# project is appropriate for that channel, and return a hashref of 
+# channel => project (leaving out channels for which no project is defined)
 sub channels_and_projects {
     my $self = shift;
     my %project_for_channel; 
     for my $channel ($self->bot->channels) {
-        if (my $project = $self->github_project($channel)) {
+        if (my $project = $self->project_for_channel($channel)) {
             $project_for_channel{$channel} = $project;
         }
     }
     return \%project_for_channel;
 }
-        
+
+# Support configuring project details for a channel (potentially with auth
+# details) via a msg.  This is a bit too tricky to just leave the Vars module to
+# handle, I think.  (Note that each of the modules which inherit from us will
+# get this method; one of them will catch it and handle it.)
+sub said {
+    my ($self, $mess, $pri) = @_;
+    return unless $pri == 2;
+    return unless $mess->{address} eq 'msg';
+    
+    if ($mess->{body} =~ m{
+        ^!setgithubproject \s+
+        (?<channel> \#\S+ ) \s+
+        (?<project> \S+   ) \s+
+        (?<auth>    \S+   )?
+    }xi) {
+        my $project_for_channel = 
+            $self->store->get('GitHub','project_for_channel') || {};
+        $project_for_channel->{$+{channel}} = $+{project};
+        $self->store->store(
+            'GitHub', 'project_for_channel', $project_for_channel
+        );
+
+        my $auth_for_channel =
+            $self->store->get('GitHub', 'auth_for_channel') || {};
+        $auth_for_channel->{$+{channel}} = $+{auth};
+        $self->store->store(
+            'GitHub', 'project_for_channel', $auth_for_channel
+        );
+
+        # Invalidate any cached Net::GitHub object we might have, so the new
+        # settings are used
+        delete $net_github{$+{channel}};
+
+    } elsif ($mess->{body} =~ /^!setgithubproject/i) {
+        return "Invalid usage.   Try '!help github'";
+    }
+    return;
+}
 
 
 =head1 NAME
@@ -85,23 +158,27 @@ shared functionality and common documentation.
 The modules above need to know what GitHub project repository they should refer
 to.
 
-You can configure a project which applies to all channels the bot is in, or
-configure a different project for each channel if the bot is in multiple
-channels.
+The project (and, optionally, authentication details, if it's not a public
+project) are configured with the C<!setgithubproject> command in a private
+message to the bot.
 
-The easiest way to change the setting is using the
-L<Bot::BasicBot::Pluggable::Module::Vars> module - with that module loaded,
-authenticate with the bot (see L<Bot::BasicBot::Pluggable::Module::Auth>) then
-send the bot a command like:
+You'll need to be authenticated to the bot in order to set the project
+(see L<the auth module|Bot::BasicBot::Pluggable::Module::Auth>).
 
-    set github_project githubusername/githubprojectname
+You set the project with:
 
-For example, for this project:
+  !setgithubproject #channel user/projectname
 
-    set gitpub_project bigpresh/Bot-BasicBot-Pluggable-Module-GitHub
+That sets the default project repo to C<projectname> owned by C<user> on GitHub
+(in other words, <https://github.com/user/projectname>).
 
-Alternatively, you can change the C<user_github_project> setting directly in the
-bot's store; see the L<Bot::BasicBot::Pluggable::Store> documentation.
+If the project is a private repository which requires authentication, you can
+also tell the bot what user and token it should use to authenticate:
+
+  !setgithubproject #channel user/privateproject someuser:githubtokenhere
+
+You can generate/find an API token for your GitHub account at
+L<https://github.com/account/admin>
 
 
 =head1 AUTHOR
